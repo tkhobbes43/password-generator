f2b67fe035927a1985447047e2ab46ac5a468a66
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-20 00:22:18 -0500

    using console.log, checked to see what if i get message in console if i press generate password button

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-let numbers = ["0123456789"];
-let symbols = ["!@#$%^&'*()_+~`|}{[]|:;?><,./-='"];
-let upperCaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-let lowerCaseLetters = ["abcdefghjklmnopqrstuvwxyz"];
-
-const characterAmount = characterAmountNumber.value
-const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-const includeUppercase = includeUppercase
-const includeUppercase = 
-const includeNumbers = 
-const include Symbols = 
+function generatePassword() {
+  console.log("Hey! You clicked the button!")
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -26,4 +17,26 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+
+// What i've added to the code so far 
+//let numbers = ["0123456789"];
+//let symbols = ["!@#$%^&*()_+~`|}{[]|:;?><,./-='"];
+//let upperCaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+//let lowerCaseLetters = ["abcdefghjklmnopqrstuvwxyz"];
+
+//const characterAmount = characterAmountNumber.value
+//const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+//const includeUppercase = 
+//const includeUppercase = 
+//const includeNumbers = 
+//const include Symbols = 
+
+
+
+// What the prompts need to say
+
+//"How many characters would you like your password to contain?"
+//"Click OK to confirm including special characters."
+//"Click OK to confirm including numbers."
+//"Click OK to confirm including lowercase characters."
+//"Click OK to confirm including uppercase characters."
