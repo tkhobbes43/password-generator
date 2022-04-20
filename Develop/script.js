bcc183ce7dd5f4a740c998480ac2fc8175635681
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-19 23:54:21 -0500

    worked on JS, trying to map out the different const for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+let numbers = ["0123456789"];
+let symbols = ["!@#$%^&'*()_+~`|}{[]|:;?><,./-='"];
+let upperCaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+let lowerCaseLetters = ["abcdefghjklmnopqrstuvwxyz"];
+
+const characterAmount = characterAmountNumber.value
+const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+const includeUppercase = includeUppercase
+const includeUppercase = 
+const includeNumbers = 
+const include Symbols = 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +25,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
