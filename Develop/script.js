17d29bf1d2b86c8a3c047396f8ea258a8bb88a24
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-21 18:53:08 -0500

    redid varaiables for characters to strings, because it seems easier to do than arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,19 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 // arrays for each conditional variable
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~', "'",'"'];
-let upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
-let allCharacters = [...numbers, ...symbols, ...upperCaseLetters, ...lowerCaseLetters]
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()+-.~|<>=-_/:;?[]{}~'";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";  
+
 
 // needed to randomize each criteria into an array
 function randomize(array) {
@@ -46,12 +51,7 @@ function generatePassword() {
   
 };
 
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
