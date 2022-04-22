4b92a63b44bfb76cf6fe90755fd7643e292901fc
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-21 22:00:09 -0500

    numberOfCharacters variable is completed

--- Develop/script.js
+++ Develop/script.js
@@ -13,14 +13,20 @@ var numbers = "0123456789";
 var symbols = "!@#$%^&*()+-.~|<>=-_/:;?[]{}~'";
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";  
-
+var numberOfCharacters = "";
 
 function generatePassword() {
- //want to have all the most of the work done here inside this function, like executing the prompt and confirms, then if user whats that condition, what that does to the passwordCharacter string
- //then user selects either ok or cancel on all the conditions, it combines all the variables into finalPassword string
-  let numberOfCharacters = 0
-  while(numberOfCharacters === 0 || numberOfCharacters < 8 || numberOfCharacters > 128) {
-    numberOfCharacters = parseInt(prompt("How many characters would you like your password to contain?"));
+ // want to have all the most of the work done here inside this function, like executing the prompt and confirms, then if user whats that condition, what that does to the passwordCharacter string
+ // then user selects either ok or cancel on all the conditions, it combines all the variables into finalPassword string
+  
+  var numberOfCharacters = prompt("How many characters would you like your password to contain?");
+  // adding if statement for this variable, so that they are picking between 8 to 128 character passwords
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    // and if they choose outside of that range, they get this alert, making them choose again so that they are selecting within desired range 
+    alert("Password must be between 8 and 128 characters.");
+    return numberOfCharacters;
+  }
+  
   }
   let includeNumbers = confirm("Click OK to confirm including numbers.")
   let specialCharacters = confirm("Click OK to confirm including special characters.")
